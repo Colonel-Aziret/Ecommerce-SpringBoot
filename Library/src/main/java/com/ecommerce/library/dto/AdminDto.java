e042778507d3001bfb1f32baffc21cb0a0328c39
Colonel-Aziret <Aziret5265@gmail.com>
2023-07-27 17:29:58 +0600

    Initial commit

---
+++ Library/src/main/java/com/ecommerce/library/dto/AdminDto.java
@@ -0,0 +1,21 @@
+package com.ecommerce.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminDto {
+    @Size(min = 3, max = 10, message = "Invalid first name!(3-10 character)")
+    private String firstName;
+    @Size(min = 3, max = 10, message = "Invalid last name!(3-10 character)")
+    private String lastName;
+    private String username;
+    @Size(min = 3, max = 10, message = "Invalid password!(5-15 character)")
+    private String password;
+    private String repeatPassword;
+}
